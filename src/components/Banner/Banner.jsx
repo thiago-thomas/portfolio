83a85a0a1c3539cf1d4d063a7f8ebb328d4d1d3e
Thiago Thomas <thiago_rocha17@hotmail.com>
2024-08-23 12:42:42 +0100

    fix: banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,28 +5,26 @@ export default function Banner({ name, role, github }) {
 
   return (
     <section id="home">
-      <section id="home">
-        <div class="sec-contents">
-          <div class="home-left">
-            <h1>
-              Hello, I'm {name} {role}
-            </h1>
-            <p>
-              Welcome to my portfolio, here you will find some of my work as a
-              {" " + role}, stay cozy!
-            </p>
-          </div>
-          <div class="home-right">
-            <div className="img-container">
-              <img
-                src={githubUserImg}
-                alt={name + " picture"}
-                class="home-right-img"
-              ></img>
-            </div>
+      <div class="sec-contents">
+        <div class="home-left">
+          <h1>
+            Hello, I'm {name} {role}
+          </h1>
+          <p>
+            Welcome to my portfolio, here you will find some of my work as a
+            {" " + role}, stay cozy!
+          </p>
+        </div>
+        <div class="home-right">
+          <div className="img-container">
+            <img
+              src={githubUserImg}
+              alt={name + " picture"}
+              class="home-right-img"
+            ></img>
           </div>
         </div>
-      </section>
+      </div>
     </section>
   );
 }
