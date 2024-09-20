c88e52d1713726100179a2d40557af7b93517458
Thiago Thomas <thiago_rocha17@hotmail.com>
2024-09-20 23:03:05 +0100

    feat: navbar collapsed and fixed

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -22,52 +22,54 @@ export default function Navigation({ logo }) {
   return (
     <>
       <nav className="navbar">
-        <div className="navbar-brand">
-          <a href="#" className="logo">
-            {logo}
-          </a>
-          <button className="hamburger" onClick={toggleMenu}>
-            {/* Ícone de menu hamburguer */}
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </button>
-        </div>
-        <div className={`nav-menu ${isOpen ? "open" : ""}`}>
-          <ul>
-            <li>
-              <a onClick={toggleMenu} href="#home">
-                {t("home")}
-              </a>
-            </li>
-            <li>
-              <a onClick={toggleMenu} href="#skills">
-                {t("skills")}
-              </a>
-            </li>
-            <li>
-              <a onClick={toggleMenu} href="#projects">
-                {t("projects")}
-              </a>
-            </li>
-          </ul>
-          <div className="social-container">
-            <div className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/thiago-thomas/"
-                target="_blank"
-              >
-                <FontAwesomeIcon icon={faLinkedin} size="xl" />
-              </a>
-              <a href="https://github.com/thiago-thomas" target="_blank">
-                <FontAwesomeIcon icon={faGithub} size="xl" />
-              </a>
-            </div>
-            <DarkModeSwitch />
+        <div className="navbar-content">
+          <div className="navbar-brand">
+            <a href="#" className="logo">
+              {logo}
+            </a>
+            <button className="hamburger" onClick={toggleMenu}>
+              {/* Ícone de menu hamburguer */}
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </button>
           </div>
-          <div className="language-container">
-            <button onClick={() => changeLanguage("en")}>English</button>
-            <button onClick={() => changeLanguage("pt")}>Português</button>
+          <div className={`nav-menu ${isOpen ? "open" : ""}`}>
+            <ul>
+              <li>
+                <a onClick={toggleMenu} href="#home">
+                  {t("home")}
+                </a>
+              </li>
+              <li>
+                <a onClick={toggleMenu} href="#skills">
+                  {t("skills")}
+                </a>
+              </li>
+              <li>
+                <a onClick={toggleMenu} href="#projects">
+                  {t("projects")}
+                </a>
+              </li>
+            </ul>
+            <div className="social-container">
+              <div className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/thiago-thomas/"
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} size="xl" />
+                </a>
+                <a href="https://github.com/thiago-thomas" target="_blank">
+                  <FontAwesomeIcon icon={faGithub} size="xl" />
+                </a>
+              </div>
+              <DarkModeSwitch />
+            </div>
+            <div className="language-container">
+              <button onClick={() => changeLanguage("en")}>English</button>
+              <button onClick={() => changeLanguage("pt")}>Português</button>
+            </div>
           </div>
         </div>
       </nav>
